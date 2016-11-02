84d12b77a40b223d55145ee5389e1155d21c7d29
Overfladen <Overfladen@DESKTOP-GLFTENO>
2016-11-02 10:57:12 +0100

    Merged with local google drive testing.

--- LectioCalanderWebPort/index.php
+++ LectioCalanderWebPort/index.php
@@ -1,5 +1,39 @@
 <?php
-include ("cURLlectio.php");
-$output = cURLlectio("http://www.lectio.dk/lectio/518/SkemaNy.aspx?type=elev&elevid=17544506163");
-echo '<pre>';print_r($output);echo '</pre>';
+require_once __DIR__.'/vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfig('client_secrets.json');
+$client->addScope(Google_Service_Calendar::CALENDAR);
+$guzzleClient = new \GuzzleHttp\Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false, ), ));
+$client->setHttpClient($guzzleClient);
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+    $client->setAccessToken($_SESSION['access_token']);
+    $service = new Google_Service_Calendar($client);
+    $calendarId = 'primary';
+    $optParams = array(
+      'maxResults' => 10,
+      'orderBy' => 'startTime',
+      'singleEvents' => TRUE,
+      'timeMin' => date('c'),
+    );
+    $results = $service->events->listEvents($calendarId, $optParams);
+
+    if (count($results->getItems()) == 0) {
+        print "No upcoming events found.\n";
+    } else {
+        print "Upcoming events:\n";
+        foreach ($results->getItems() as $event) {
+            $start = $event->start->dateTime;
+            if (empty($start)) {
+                $start = $event->start->date;
+            }
+            printf("%s (%s)\n", $event->getSummary(), $start);
+        }
+    }
+} else {
+    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+    header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
 ?>
---
+++ LectioCalanderWebPort/oauth2callback.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfigFile('client_secrets.json');
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
+$client->addScope(Google_Service_Calendar::CALENDAR);
+$guzzleClient = new \GuzzleHttp\Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false, ), ));
+$client->setHttpClient($guzzleClient);
+if (! isset($_GET['code'])) {
+    $auth_url = $client->createAuthUrl();
+    header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+} else {
+    $client->authenticate($_GET['code']);
+    $_SESSION['access_token'] = $client->getAccessToken();
+    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
+?>
