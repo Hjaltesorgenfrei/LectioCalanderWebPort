ce910797968ce9869fedcdd22981785a255b6d7c
Overfladen <Overfladen@DESKTOP-GLFTENO>
2016-11-02 10:32:46 +0100

    Fixed regex for this time.

---
+++ LectioCalanderWebPort/cURLlectio.php
@@ -0,0 +1,15 @@
+<?php
+function cURLlectio($url){
+    $url = "$url";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); //Doesnt verify the servers ssl. As it could make it fail if lectio's ssl runs out.
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    $regexpattern = '((\btitle[=]\")(.*(?<!Aflyst\!)\n)?(.*(?<!Aflyst\!)\n)?((\d{1,2})\/(\d{1,2})-(\d{4})\s(\d{2}):(\d{2})\s\btil\s(\d{2}):(\d{2}))\n(\bHold:.*?\n.*?\n.*?)?\n)';
+    preg_match_all($regexpattern, $output, $regexresults);
+    echo '<pre>';print_r($regexresults);echo '</pre>';
+    return $regexresults;
+}
+?>
